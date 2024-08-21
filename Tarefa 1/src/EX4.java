2f31c8add0210f32ccf853538fa42f95473f6e30
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-08-21 11:48:53 -0300

    Update EX4.java

--- Tarefa 1/src/EX4.java
+++ Tarefa 1/src/EX4.java
@@ -14,3 +14,15 @@ public class EX4 {
         System.out.println("O custo final ao consumidor é R$:" + custo_final);
     }
 }
+
+/* Teste de Mesa:
+
+| Passo | Descrição                                | custo_fabrica | percentual_distribuidor | impostos | custo_final |                       Cálculo                                | Resultado                          |
+|-------|------------------------------------------|---------------|-------------------------|----------|-------------|--------------------------------------------------------------|------------------------------------|
+| 1     | Entrada: Digite o custo de fábrica       | 5000          |                         |          |             |                                                              |                                    |
+| 2     | Calcular o percentual do distribuidor    | 5000          |                         |          |             | `custo_fabrica * 28 / 100`                                   | 5000 * 28 / 100 = 1400.0           |
+| 3     | Calcular os impostos                     | 5000          | 1400.0                  |          |             | `custo_fabrica * 45 / 100`                                   | 5000 * 45 / 100 = 2250.0           |
+| 4     | Calcular o custo final                   | 5000          | 1400.0                  | 2250.0   |             | `custo_fabrica + percentual_distribuidor + impostos`         | 5000 + 1400 + 2250 = 8650.0        |
+| 5     | Exibir o custo final                     | 5000          | 1400.0                  | 2250.0   | 8650.0      |                                                              | "O custo final ao consumidor é R$: 8650.0"|
+
+*/
