cb91725d1a5413bf73700df9823fc3f948fd9482
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-08-20 21:17:59 -0300

    Update EX1.java

--- Tarefa 1/src/EX1.java
+++ Tarefa 1/src/EX1.java
@@ -17,3 +17,15 @@ public class EX1 {
         scanner.close();
     }
 }
+
+/*Teste de mesa:
+
+| Passo | Descrição                            | anos | meses | a_dias | m_dias | dias | Cálculo                | Resultado |
+|-------|--------------------------------------|------|-------|--------|--------|------|------------------------|-----------|
+| 1     | Entrada: Idade em anos               | 2    |       |        |        |      |                        |           |
+| 2     | Calcular meses baseados nos anos     | 2    | 24    |        |        |      | `anos * 12`            | 24        |
+| 3     | Calcular dias baseados nos anos      | 2    | 24    | 730    |        |      | `anos * 365`           | 730       |
+| 4     | Calcular dias baseados nos meses     | 2    | 24    | 730    | 720    |      | `meses * 30`           | 720       |
+| 5     | Adicionar dias diretamente           | 2    | 24    | 730    | 720    | 10   |                        | 10        |
+| 6     | Somar todos os dias                  | 2    | 24    | 730    | 720    | 10   | `730 + 720 + 10`       | 1460      |
+| 7     | Resultado final                      | 2    | 24    | 730    | 720    | 10   |                        | 1460      |*/
