35027e7b21da0c206f31ca418ca9855fa7a1ea60
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-08-21 11:30:27 -0300

    Update EX16.java

--- Tarefa 1/src/EX16.java
+++ Tarefa 1/src/EX16.java
@@ -16,3 +16,16 @@ public class EX16 {
 
         }
     }
+/* Teste de Mesa:
+
+| Passo | Descrição                            | janeiro | fev  | marco | gasto_total | media    | Cálculo                        | Resultado           |
+|-------|--------------------------------------|---------|------|-------|-------------|----------|--------------------------------|---------------------|
+| 1     | Entrada: Valor de janeiro            | 15000   |      |       |             |          |                                |                     |
+| 2     | Entrada: Valor de fevereiro          | 15000   | 23000|       |             |          |                                |                     |
+| 3     | Entrada: Valor de março              | 15000   | 23000| 17000 |             |          |                                |                     |
+| 4     | Calcular o gasto total               | 15000   | 23000| 17000 | 55000       |          | `janeiro + fev + marco`        | 55000               |
+| 5     | Calcular a média                     | 15000   | 23000| 17000 | 55000       | 18333.33 | `gasto_total / 3`              | 18333.33            |
+| 6     | Exibir o gasto total                 | 15000   | 23000| 17000 | 55000       | 18333.33 |                                | "O gasto total é de R$: 55000" |
+| 7     | Exibir a média do gasto              | 15000   | 23000| 17000 | 55000       | 18333.33 |                                | "E a média desse gasto é de 18333.33 %" |
+
+*/
