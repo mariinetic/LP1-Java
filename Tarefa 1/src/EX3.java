4a5c17375d71e4a1d550b4cc5c6f321951598ba5
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-08-21 11:46:58 -0300

    Update EX3.java

--- Tarefa 1/src/EX3.java
+++ Tarefa 1/src/EX3.java
@@ -16,3 +16,15 @@ public class EX3 {
         scanner.close();
     }
 }
+/* Teste de Mesa:
+
+| Passo | Descrição                                | salario | reajuste | novo_salario |                     Cálculo                                |            Resultado              |
+|-------|------------------------------------------|---------|----------|--------------|------------------------------------------------------------|-----------------------------------|
+| 1     | Entrada: Digite o seu salário mensal     | 3000    |          |              |                                                            |                                   |
+| 2     | Entrada: Digite o percentual de reajuste | 3000    | 10       |              |                                                            |                                   |
+| 3     | Calcular o valor do reajuste             | 3000    | 10       |              | `salario * reajuste`                                       | 3000 * 10 = 30000                 |
+| 4     | Dividir o valor do reajuste por 100      | 3000    | 10       |              | `30000 / 100`                                              | 30000 / 100 = 300.0               |
+| 5     | Calcular o novo salário                  | 3000    | 10       | 3300.0       | `salario + (salario * reajuste / 100)`                     | 3000 + 300 = 3300.0               |
+| 6     | Exibir o novo salário                    | 3000    | 10       | 3300.0       |                                                            | "Seu novo salário é R$: 3300.0"   |
+
+*/
