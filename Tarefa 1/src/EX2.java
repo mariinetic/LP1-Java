78666ca0ee3d96aab6c6d3b1312925d5a0f38b86
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-08-21 11:43:41 -0300

    Update EX2.java

--- Tarefa 1/src/EX2.java
+++ Tarefa 1/src/EX2.java
@@ -27,3 +27,20 @@ public class EX2 {
         scanner.close();
     }
 }
+
+/* Teste de Mesa:
+
+| Passo | Descrição                                    | eleitores | voto_branco | voto_nulo | voto_valido | percentual_vb | percentual_vn | percentual_vv |                    Cálculo                      |        Resultado           |
+|-------|----------------------------------------------|-----------|-------------|-----------|-------------|---------------|---------------|---------------|-------------------------------------------------|----------------------------|
+| 1     | Entrada: Número dos eleitores totais         | 1000      |             |           |             |               |               |               |                                                 |                            |
+| 2     | Entrada: Número de votos brancos             | 1000      | 200         |           |             |               |               |               |                                                 |                            |
+| 3     | Entrada: Número de votos nulos               | 1000      | 200         | 100       |             |               |               |               |                                                 |                            |
+| 4     | Entrada: Número de votos válidos             | 1000      | 200         | 100       | 700         |               |               |               |                                                 |                            |
+| 5     | Calcular o percentual de votos brancos       | 1000      | 200         | 100       | 700         | 20.0%         |               |               | `(voto_branco / eleitores) * 100`               | 20.0%                      |
+| 6     | Calcular o percentual de votos nulos         | 1000      | 200         | 100       | 700         | 20.0%         | 10.0%         |               | `(voto_nulo / eleitores) * 100`                 | 10.0%                      |
+| 7     | Calcular o percentual de votos válidos       | 1000      | 200         | 100       | 700         | 20.0%         | 10.0%         | 70.0%         | `(voto_valido / eleitores) * 100`               | 70.0%                      |
+| 8     | Exibir o percentual de votos brancos         | 1000      | 200         | 100       | 700         | 20.0%         | 10.0%         | 70.0%         |                                                 | "O percentual dos votos brancos são: 20.0%" |
+| 9     | Exibir o percentual de votos nulos           | 1000      | 200         | 100       | 700         | 20.0%         | 10.0%         | 70.0%         |                                                 | "O percentual dos votos nulos são: 10.0%"   |
+| 10    | Exibir o percentual de votos válidos         | 1000      | 200         | 100       | 700         | 20.0%         | 10.0%         | 70.0%         |                                                 | "O percentual dos votos válidos são: 70.0%" |
+
+*/
