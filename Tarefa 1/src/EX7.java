d28450ad93cba3ea523422cfaaff02618cc14b3a
Mariana Oliveira <162724918+mariinetic@users.noreply.github.com>
2024-08-21 19:03:30 -0300

    Update EX7.java

--- Tarefa 1/src/EX7.java
+++ Tarefa 1/src/EX7.java
@@ -13,3 +13,12 @@ public class EX7 {
         scanner.close();
     }
 }
+/* Teste de Mesa:
+
+| Passo | Descrição                                   | valor | resultado              |                        Cálculo                                   | Resultado                     |
+|-------|---------------------------------------------|-------|------------------------|------------------------------------------------------------------|-------------------------------|
+| 1     | Entrada: Digite um valor                    | 15    |                        |                                                                  |                               |
+| 2     | Avaliar se o valor é maior que 10           | 15    |                        | `(valor > 10) ? "É maior que 10!" : "Não é maior que 10!"`       | "É maior que 10!"             |
+| 3     | Exibir o resultado                          | 15    | "É maior que 10!"      |                                                                  | "É maior que 10!"             |
+
+*/
